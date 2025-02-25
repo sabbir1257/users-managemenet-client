3d821975c0825113ede5e6b5805e6d74b53a0cf4
sabbir1257 <masabbir1257@gmail.com>
2025-02-25 11:38:52 +0600

    Create React form and Post API and send data to the server

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,48 @@ import { useEffect } from "react";
 function App() {
   const [users, setUsers] = useState([]);
 
- useEffect(() => {
-  fetch('http://localhost:5000/users')
-  .then(res => res.json())
-  .then(data => setUsers(data))
- } ,[])
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+  };
 
   return (
     <>
       <h1>Users Management System</h1>
       <h3>Number of Users: {users.length}</h3>
+
+      <form onSubmit={handleAddUser}>
+        <input type="name" name="name" id="" />
+        <br />
+        <input type="email" name="email" />
+        <br />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+
       <div>
-        {
-          users.map(user => <p key={user.id}>{user.id} : {user.name} : {user.email}</p>)
-        }
+        {users.map((user) => (
+          <p key={user.id}>
+            {user.id} : {user.name} : {user.email}
+          </p>
+        ))}
       </div>
     </>
   );
